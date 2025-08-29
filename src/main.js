90ae565fd1c9a3b72c6cc5a97044b4b197b790f1
Maksimsemikras <maksim.semikras@mail.ru>
2025-08-30 00:49:59 +0300

    sohranenie

--- src/main.js
+++ src/main.js
@@ -61,8 +61,8 @@ function analyzeSalesData(data, options) {
 
     // @TODO: Проверка наличия опций
     if (!typeof options !== "object"
-    || !typeof calculateRevenue !== "function"
-    ||!typeof calculateBonus !== "function") {
+    || typeof calculateRevenue !== "function"
+    || typeof calculateBonus !== "function") {
         throw new Error("Неверные параметры")
     }
 
